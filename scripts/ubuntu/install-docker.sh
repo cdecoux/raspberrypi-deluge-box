14437f66b98d1fababf80112b8d2bbccb6972947
Caleb DeCoux <cdecoux@firstorion.com>
2022-02-18 15:44:06 -0600

    installed docker-compose

--- scripts/ubuntu/install-docker.sh
+++ scripts/ubuntu/install-docker.sh
@@ -17,3 +17,9 @@ echo \
 
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Compose v2 Plugin
+COMPOSE_VERSION="v2.2.3"
+mkdir -p /usr/libexec/docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-linux-x86_64 -o /usr/libexec/docker/cli-plugins/docker-compose
+chmod +x /usr/libexec/docker/cli-plugins/docker-compose
